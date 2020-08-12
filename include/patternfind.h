210deb9a1e08486f0975e6f1debddc0a49b786f7
pilao <zeffy@users.noreply.github.com>
2020-08-12 02:20:52 -0700

    xd

--- include/patternfind.h
+++ include/patternfind.h
@@ -1,9 +1,22 @@
 #pragma once
-
 #include <vector>
 
-namespace patternfind
+namespace catgirls
 {
+  namespace
+  {
+    inline int hexchtoint(char ch)
+    {
+      if ( ch >= '0' && ch <= '9' )
+        return ch - '0';
+      else if ( ch >= 'A' && ch <= 'F' )
+        return ch - 'A' + 10;
+      else if ( ch >= 'a' && ch <= 'f' )
+        return ch - 'a' + 10;
+      return -1;
+    }
+  }
+
   struct patternbyte
   {
     unsigned char value;
@@ -11,10 +24,11 @@ namespace patternfind
 
     patternbyte()
     {
-      this->clear();
+      this->value = 0;
+      this->mask = 0;
     }
 
-    patternbyte(const unsigned char value, const unsigned char mask)
+    patternbyte(const unsigned char value, const unsigned char mask = 0xff)
     {
       this->value = value;
       this->mask = mask;
@@ -22,55 +36,41 @@ namespace patternfind
 
     inline bool opaque() const
     {
-      return this->mask == 0xffui8;
-    }
-
-    inline void clear()
-    {
-      this->value = 0ui8;
-      this->mask = 0xffui8;
+      return this->mask == 0xff;
     }
 
     inline bool operator==(const unsigned char &rhs) const
     {
       return (rhs & this->mask) == (this->value & this->mask);
     }
+
+    friend inline bool operator==(const unsigned char &lhs, const patternbyte &rhs)
+    {
+      return (lhs & rhs.mask) == (rhs.value & rhs.mask);
+    }
   };
 
-  inline bool operator==(const unsigned char &lhs, const patternbyte &rhs) 
+  static std::vector<catgirls::patternbyte> compile(const char *pattern)
   {
-    return (lhs & rhs.mask) == (rhs.value & rhs.mask);
-  }
-
-  inline int hexchtoint(char ch)
-  {
-    if ( ch >= '0' && ch <= '9' )
-      return ch - '0';
-    else if ( ch >= 'A' && ch <= 'F' )
-      return ch - 'A' + 10;
-    else if ( ch >= 'a' && ch <= 'f' )
-      return ch - 'a' + 10;
-    return -1;
-  }
-
-  static std::vector<patternbyte> compile(const char *pattern)
-  {
-    std::vector<patternbyte> v;
-    patternbyte b;
+    std::vector<catgirls::patternbyte> v;
+    catgirls::patternbyte b;
     char c;
     int shift = 4;
 
     while ( c = *pattern++ ) {
       if ( c == '?' || c == '.' )
         b.mask &= ~(0xf << shift);
-      else if ( auto num = hexchtoint(c); num != -1 )
+      else {
+        const int num = hexchtoint(c);
+        if ( num < 0 )
+          continue;
         b.value |= (num & 0xf) << shift;
-      else
-        continue;
+      }
 
       if ( !shift ) {
         v.push_back(b);
-        b.clear();
+        b.value = 0;
+        b.mask = 0xff;
       }
       shift ^= 4;
     }
